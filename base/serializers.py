5a6c2a17cd3cbf6ef1e32886e5922feb08f2047a
masasandeep <masasandeep12@gmail.com>
2023-09-09 15:58:16 +0530

    serializers

--- base/serializers.py
+++ base/serializers.py
@@ -7,14 +7,14 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'password', 'email','Allergies','Health concerns')
+        fields = ('id', 'username', 'password', 'email','first_name', 'last_name')
 
     def create(self, validated_data):
         user = User.objects.create(
             username=validated_data['username'],
             email=validated_data['email'],
-            first_name=validated_data['Allergies'],
-            last_name=validated_data['Health concerns'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
         )
         user.set_password(validated_data['password'])
         user.save()
