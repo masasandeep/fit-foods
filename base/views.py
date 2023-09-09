1ffbcbac4c2f81cf2b7e1cbda077cb4f00137acb
gunavardhan27 <bgunavardhan27@gmail.com>
2023-09-09 22:52:52 +0530

    fork

--- base/views.py
+++ base/views.py
@@ -5,8 +5,13 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.views import APIView
-from .serializers import UserSerializer,GetSerializer
+<<<<<<< HEAD
+from .serializers import UserSerializer,GetSerializer,AllergySerializer,HealthSerializer
 from rest_framework.decorators import api_view
+=======
+from .serializers import UserSerializer,GetSerializer
+from rest_framework.decorators import api_view, permission_classes
+>>>>>>> 49dacee1cad9628a627846766e23c4499d847afd
 from django.contrib.auth import authenticate
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
@@ -62,3 +67,41 @@ class Display(APIView):
         user = User.objects.all()
         serial = GetSerializer(user,many=True)
         return Response(serial.data)
+<<<<<<< HEAD
+    
+    
+class Allergy(APIView):
+    def get(self,request):
+        data = Allergies.objects.all()
+        serializer = AllergySerializer(data,many=True)
+        return Response(serializer.data)
+    
+class Health(APIView):
+    def get(self,request):
+        data = HealthIssue.objects.all()
+        serializer = HealthSerializer(data,many=True)
+        return Response(serializer.data)
+=======
+# Django views.py
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def update_username(request):
+    new_username = request.data.get('new_username')
+    if new_username:
+        request.user.username = new_username
+        request.user.save()
+        return Response({'message': 'Username updated successfully'}, status=status.HTTP_200_OK)
+    else:
+        return Response({'error': 'New username is required'}, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def update_password(request):
+    new_password = request.data.get('new_password')
+    if new_password:
+        request.user.set_password(new_password)
+        request.user.save()
+        return Response({'message': 'Password updated successfully'}, status=status.HTTP_200_OK)
+    else:
+        return Response({'error': 'New password is required'}, status=status.HTTP_400_BAD_REQUEST)
+>>>>>>> 49dacee1cad9628a627846766e23c4499d847afd
