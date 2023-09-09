979c890def42ef4c4f6a5003f24e21f2133eac1f
ganeshpilla <ganeshpilla98765@gmail.com>
2023-09-10 03:34:09 +0530

    Search

--- food-app-react-tailwind-main/src/components/FoodDisplay.jsx
+++ food-app-react-tailwind-main/src/components/FoodDisplay.jsx
@@ -11,29 +11,32 @@ function FoodData() {
 
   return (
     <>
-    <NavBar/>
-    <div className="flex flex-row justify-between item-center bg-gradient-to-r from-black to-gray-800">
-      <div className="bg-grey">
-        <div className="grid grid-cols-1 item-center py-10 px-10 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      <NavBar />
+      <div className="flex flex-row justify-between item-center bg-gradient-to-r from-black to-gray-800">
 
-          {/* {foods.map((food, index) => ( */}
-    </>
-          if(location.state.found)
-          {
-            <FoodCard food={location.state.item1} />
-          }
-          else{
-            
-              FoodList.data.map((food, index) => (
-                <FoodCard food={food} key={index} />
-              ))
+        {/* {foods.map((food, index) => ( */}
+        if(location.state.found)
+        {
+
+          <FoodCard food={location.state.item1} />
+        }
+        else{
+          <div className="bg-grey">
+            <div className="grid grid-cols-1 item-center py-10 px-10 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+           {
+            FoodList.data.map((props, index) => { (
+              <FoodCard food={props} key={index} />
+              )})
             }
-          
+            </div>
+          </div>
+        }
 
-          {/* ))} */}
-        </div>
+
+        {/* ))} */}
+
       </div>
-    </div>
+    </>
   );
 }
 
