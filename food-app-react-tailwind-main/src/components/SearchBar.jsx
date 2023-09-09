ed61a4de8446123763ffbfc7559b7c2d751ba0e8
ganeshpilla <ganeshpilla98765@gmail.com>
2023-09-10 03:24:30 +0530

    frontend

--- food-app-react-tailwind-main/src/components/SearchBar.jsx
+++ food-app-react-tailwind-main/src/components/SearchBar.jsx
@@ -3,26 +3,31 @@ import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 import makeStyles from '@mui/styles/makeStyles';
 import styled from 'styled-components';
+import FoodCard from './FoodCard';
+import FoodDisplay from './FoodDisplay';
+import FoodList from '../backend/FoodList.json';
 import {
     AiOutlineMenu,
     AiOutlineSearch,
     AiOutlineClose,
     AiFillTag,
-  } from "react-icons/ai";
+} from "react-icons/ai";
+import { Button } from '@mui/material';
+import { Link,useNavigate } from 'react-router-dom';
 
 const suggestions = [
-  'Apple',
-  'Banana',
-  'Cherry',
-  'Date',
-  'Elderberry',
+    'Apple',
+    'Banana',
+    'Burger',
+    'Pizza',
+    'Dates',
 ];
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%', // Set the width to 100% to fill the available space
-  },
-  
+    root: {
+        width: '100%', // Set the width to 100% to fill the available space
+    },
+
 }));
 
 const StyledAutocomplete = styled(Autocomplete)`
@@ -39,29 +44,50 @@ const StyledTextField = styled(TextField)`
 `;
 
 function SearchBar() {
-  const classes = useStyles();
-  const [value, setValue] = useState('');
-
-  return (
-    // <div className="bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]">
+    const nav=useNavigate();
+    const classes = useStyles();
+    const [value, setValue] = useState('');
+    const [found,setFound]=useState(false);
+    const handleClick=()=>{
+        
+        FoodList.data.map((item1)=>{if(item1.name===value){
+            // console.log(item1);
+            // nav("/FoodDisplay");
+            setFound(true);
+            nav('/FoodDisplay',{state:{item1:item1,found:found}});
+        }
+        else
+        {
+            setFound(false);
+            nav('/FoodDisplay',{state:{item1:item1,found:found}});
+        }
+    })
+    }
+    return (
+        // <div className="bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]">
         // <AiOutlineSearch size={25} />
-    <StyledAutocomplete
-      options={suggestions}
-      getOptionLabel={(option) => option}
-      value={value}
-      onChange={(_, newValue) => setValue(newValue)}
-      renderInput={(params) => (
-        <StyledTextField
-          {...params}
-          label="Search for fruits"
-          variant="outlined"
-          className={classes.root}
-          style={{width:'470px',borderRadius:'20px !important'}}
-        />
-      )}
-    />
-    // </div>
+        <div className="flex flex-row justify-between items-center">
+            <Button onClick={handleClick}>
+            <AiOutlineSearch size={25}  className="mr-4" />
+             </Button>
+            <StyledAutocomplete
+                options={suggestions}
+                getOptionLabel={(option) => option}
+                value={value}
+                onChange={(_, newValue) => setValue(newValue)}
+
+                renderInput={(params) => (
+                    <StyledTextField
+                        {...params}
+                        label="Search for fruits"
+                        variant="outlined"
+                        className={classes.root}
+                        style={{ width: '470px', borderRadius: '20px !important' }}
+                    />
+                   )}
+            />
+        </div>
   );
-}  
+}
 
 export default SearchBar;
